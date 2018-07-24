88be02c734e4717b901ca76ec2fcaba1aaef5637
Simon Chuu <simonorj@outlook.com>
2018-07-23 22:54:16 -0400

    Fix exception type

--- src/main/java/ArcaneGreylistTest.java
+++ src/main/java/ArcaneGreylistTest.java
@@ -13,7 +13,7 @@ public class ArcaneGreylistTest {
 
         try {
             port = Integer.parseInt(args[0]);
-        } catch (NumberFormatException | NullPointerException e) {
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
             System.out.println("Port number required.");
             return;
         }
@@ -22,23 +22,37 @@ public class ArcaneGreylistTest {
             ServerSocket sock = new ServerSocket(port);
             HashSet<String> set = new HashSet<>();
 
+            System.out.println("Socket server listening on port " + port + ".");
+
             while (true){
                 //init the client
                 try (Socket incoming = sock.accept()) {
                     DataInputStream dis = new DataInputStream(incoming.getInputStream());
-                    String action = dis.readUTF();
-                    String user = dis.readUTF();
-
                     DataOutputStream dos = new DataOutputStream(incoming.getOutputStream());
+
+                    String action = dis.readUTF();
+
 
                     if (!action.equals("greylist")) {
                         dos.writeBoolean(false);
-                        dos.writeUTF("Action unsupported. :(");
-                    } else if (!set.add(user)) {
+                        String msg = action + " action unsupported. :(";
+                        dos.writeUTF(msg);
+                        System.out.println("Sent: " + msg);
+                        continue;
+                    }
+
+                    String user = dis.readUTF();
+
+                    if (!set.add(user)) {
                         dos.writeBoolean(false);
-                        dos.writeUTF("User is already greylisted!");
+                        String msg = user + " user is already greylisted!";
+                        dos.writeUTF(msg);
+                        System.out.println("Sent: " + msg);
+                        continue;
                     } else {
                         dos.writeBoolean(true);
+                        System.out.println("Successfully greylisted " + user);
+                        continue;
                     }
 
                 } catch (SocketException e) {
